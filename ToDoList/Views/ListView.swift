0e7aff8ed254ff9b42a28962405f27e3823b3664
Aziz <aziz.khadjayev070@gmail.com>
2022-11-09 23:16:50 +0100

    SW-01 Use ontopguesture on item

--- ToDoList/Views/ListView.swift
+++ ToDoList/Views/ListView.swift
@@ -17,7 +17,7 @@ struct ListView: View {
                 ListRowView(item: item)
                     .onTapGesture {
                         withAnimation(.linear) {
-                            
+                            listViewModel.updateItem(item: item)
                         }
                     }
             }
