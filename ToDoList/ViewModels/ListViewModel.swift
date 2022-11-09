f0354521c2a464d5aef5b7375ed4cfacd31f241c
Aziz <aziz.khadjayev070@gmail.com>
2022-11-09 23:16:22 +0100

    SW-01 Create onclick item complete

--- ToDoList/ViewModels/ListViewModel.swift
+++ ToDoList/ViewModels/ListViewModel.swift
@@ -37,4 +37,10 @@ class ListViewModel: ObservableObject {
         
         items.append(newItem)
     }
+    
+    func updateItem (item: ItemModel) {
+        if let index = items.firstIndex(where: { $0.id == item.id }){
+            items[index] = item.updateComplition()
+        }
+    }
 }
--- ToDoList/Views/ListView.swift
+++ ToDoList/Views/ListView.swift
@@ -15,6 +15,11 @@ struct ListView: View {
         List {
             ForEach(listViewModel.items) { item in
                 ListRowView(item: item)
+                    .onTapGesture {
+                        withAnimation(.linear) {
+                            
+                        }
+                    }
             }
             .onDelete(perform: listViewModel.onDelteItem)
             .onMove(perform: listViewModel.onMoveItem)
