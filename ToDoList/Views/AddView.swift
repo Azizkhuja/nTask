60533768b608ef35576473107217920850cb9115
Aziz <aziz.khadjayev070@gmail.com>
2022-11-10 13:29:51 +0100

    SW-01 Change view on landscape mode

--- ToDoList/Views/AddView.swift
+++ ToDoList/Views/AddView.swift
@@ -21,7 +21,7 @@ struct AddView: View {
                 TextField("Type something here", text: $textFieldText)
                     .padding(.horizontal)
                     .frame(height: 55)
-                    .background(Color.mint)
+                    .background(Color(UIColor.secondarySystemBackground))
                     .cornerRadius(10)
                 
                 Button(action: saveButtonPressed, label: {
